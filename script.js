261844dac0810b743fceee189ce92d4f72ba186b
frontenddev-himanshu <rajh5343@gmail.com>
2024-12-24 13:17:40 +0530

    add

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 AOS.init();
+
+
+
+// login
+let loginPage = document.querySelector(".login-page")
+let onOff = document.querySelector(".cut-bar")
+
+let navLogin = document.querySelector("#nav-login")
+
+
+// function
+
+function login() {
+
+    loginPage.style.opacity = "1";
+    loginPage.style.visibility = "visible"
+
+}
+
+setTimeout(login, 2000);
+
+
+// btn
+onOff.addEventListener("click", function () {
+
+    loginPage.style.opacity = "0";
+    loginPage.style.visibility = "hidden"
+
+});
+
+// navLogin
+navLogin.addEventListener("click", function () {
+
+    loginPage.style.opacity = "1";
+    loginPage.style.visibility = "visible"
+
+});
